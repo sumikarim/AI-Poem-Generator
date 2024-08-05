ea00d09c87eedbd7d6811a6159be32c3753b4d8a
sumikarim <sumairapkarim@gmail.com>
2024-08-05 13:49:12 +0100

    Code for AI Integration

--- index.js
+++ index.js
@@ -1,12 +1,26 @@
-function generatePoem(event) {
-  event.preventDefault();
-
+function displayPoem(response) {
+  console.log("poem generated");
   new Typewriter("#poem", {
-    strings: "This is a poem",
+    strings: response.data.answer,
     autoStart: true,
     delay: 1,
     cursor: "",
   });
+}
+
+function generatePoem(event) {
+  event.preventDefault();
+
+  let poemTheme = document.querySelector("#poem-theme");
+  let apiKey = "7502o08039672a12etf0b0b4e6112ba9";
+  let context = `Generate a short poem about ${poemTheme.value}`;
+  let prompt =
+    "You are an expert at writing poems. Please write short poem in basic HTML. Do not include the title of the poem. At the end of the poem sign 'The AI Poem Generator' in a <strong> element.";
+  let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
+
+  console.log("generating a poem");
+
+  axios.get(apiUrl).then(displayPoem);
 }
 
 let poemElement = document.querySelector("#poem-form");
